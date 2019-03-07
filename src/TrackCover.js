65a29d3b327fd4a7ddf5df7d2e522b49cd877123
Monika Ptaszek <monika.ptaszek@synergycodes.com>
2019-03-07 13:35:58 +0100

    move getPicute method to utlis

--- src/TrackCover.js
+++ src/TrackCover.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import music from './music.png';
 import { loadTrack } from './actions/trackActions';
 import { connect } from 'react-redux';
-import { getTrack } from './Utils';
+import { getTrack, getPicture } from './Utils';
 
 const TrackCover = (props) => (
     <div 
@@ -12,19 +11,6 @@ const TrackCover = (props) => (
     </div>
 );
 
-const getPicture = (picture) => {
-    if(!picture)
-        return music;
-
-    const byteArray = new Uint8Array(picture.data);
-    const oMyBlob = new Blob(
-        [byteArray], 
-        {type : picture.format }
-    );
-    const url = URL.createObjectURL(oMyBlob);
-
-    return url;
-}
 
 const mapStateToPops = (state, props) => {
     const track = getTrack(state, props.isLeft);
